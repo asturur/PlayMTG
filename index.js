7a413965b213042071fd1c6ee6aff44d65d20d1d
Andrea Bogazzi <andreabogazzi79@gmail.com>
2014-10-06 11:46:51 +0200

    Update index.js
    Cambio da virgolette doppie a virgolette singole.
    In caso di dovere mettere HTML sono piu comode. ( cosi non si mischiano i 2 tipi di virgolette )

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 'use strict';
 $(document).ready(function() {
-  $("#register2").click(registerShow);
-  $("#register").click(register);
-  $("#login2").click(loginShow);
-  $("#login").click(login);
-  $("#loginForm").show();
+  $('#register2').click(registerShow);
+  $('#register').click(register);
+  $('#login2').click(loginShow);
+  $('#login').click(login);
+  $('#loginForm').show();
 });
 
 var registerShow = function() {
-  $("#loginForm").hide();
-  $("#registerForm").show();
+  $('#loginForm').hide();
+  $('#registerForm').show();
 },
 
 loginShow = function() {
-  $("#loginForm").show();
-  $("#registerForm").hide();
+  $('#loginForm').show();
+  $('#registerForm').hide();
 },
 
 register = function() {
   $.ajax({
-    type: "POST",
-    url: "services/register.php",
-    data: $("#registerForm").serialize(),
+    type: 'POST',
+    url: 'services/register.php',
+    data: $(''#registerForm').serialize(),
     success: function(data){
       console.debug('chiamato');
       alert(data);
