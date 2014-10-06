105a4f02583dc4933741a4113117c8cb551b4d37
Andrea Bogazzi <andreabogazzi79@gmail.com>
2014-10-06 12:25:37 +0200

    Update register.php
    scheletro pagina registrazione

--- services/register.php
+++ services/register.php
@@ -1,1 +1,25 @@
-<?php var_dump($_POST); ?>
+<?php
+require "../include/database.php";
+require "../include/mail.class.php";
+
+$stmt=$mysql->prepare("INSERT INTO users (username, name, email, password) VALUES (?, ?, ?, ENCRYPT(?))");
+$stmt->bind_param('ssss', $_POST["username"], $_POST["nome"],  $_POST["email"], $_POST["password"]);
+
+$risposta = array();
+
+if ($stmt->execute()) {
+  //se inserito correttamente inviare la mail di conferma con link attivazione.
+  $mail->new mia_mail();
+  $mail->addTo($_POST["email"], $_POST["nome"]);
+  $mail->Subject = "Benvenuto su Play MTG online.";
+  $mail->html = "blah bla blah in HTML";
+  $mail->Invia();
+  $risposta["email"] = $_POST["email"];
+  $risposta["success"] = true;
+} else {
+  $risposta["success"] = false;
+  $risposta["messaggio"] = $stmt->error;
+  //restituire un messaggio di errore qualsiasi
+}
+echo json_encode($risposta);
+?>
